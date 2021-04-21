d1fae0496df298c3f63a13ff8e08072827b3e150
khanhafe <khanhafe@gmail.com>
2021-04-21 11:14:44 +0530

    callback using props

---
+++ src/Formsubmit.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Formsubmit extends React.Component {
+  state = {
+    term: ""
+  };
+
+  formsubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.formsubmit}>
+          <input
+            type="text"
+            value={this.state.term}
+            onChange={(e) => this.setState({ term: e.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+export default Formsubmit;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-
+import Formsubmit from "./Formsubmit";
 //const App = () => <div>hello world</div>;
 
 class App extends React.Component {
+  appForm = (param) => {
+    console.log(param);
+  };
   render() {
     return (
       <div>
         <SeasonDisplay />
+        <Formsubmit onSubmit={this.appForm} />
       </div>
     );
   }
